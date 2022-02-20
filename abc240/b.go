01768e8ac9ebae4da4f04eafcfb4b70ef8a0c882
Nix <vrcnix@gmail.com>
2022-02-20 23:14:11 +0900

    [update] 240 a, b fix

--- abc240/b.go
+++ abc240/b.go
@@ -18,29 +18,28 @@ func nextInt() int {
 	return i
 }
 
-func nextStr() string {
-	sc.Scan()
-	return sc.Text()
+// 数値配列をuniqして返す
+func uniq(input []int) (uniq []int) {
+	m := make(map[int]bool)
+	for _, elm := range input {
+		if !m[elm] {
+			m[elm] = true
+			uniq = append(uniq, elm)
+		}
+	}
+	return uniq
 }
 
 func main() {
 	sc.Split(bufio.ScanWords)
-	n := nextInt()
-	input := make([]int, n)
-	uniq := []int{}
+	n := nextInt()          // 入力数値総数
+	input := make([]int, n) // 入力数値配列
 
 	for i := 0; i < n; i++ {
 		input[i] = nextInt()
 	}
 
-	m := make(map[int]bool, n)
-
-	for _, elem := range input {
-		if !m[elem] {
-			m[elem] = true
-			uniq = append(uniq, elem)
-		}
-	}
-
+	// 数値のユニーク個数を出力
+	uniq := uniq(input)
 	fmt.Println(len(uniq))
 }
