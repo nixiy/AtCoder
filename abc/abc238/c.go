d28ec9920e72a741b3101e288c05883719fff404
Nix <vrcnix@gmail.com>
2022-02-23 19:03:07 +0900

    [add] abc238 c x

--- abc/abc238/c.go
+++ abc/abc238/c.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"math"
-	"math/big"
 	"os"
 	"strconv"
 )
@@ -17,55 +16,12 @@ func nextInt() int {
 	return i
 }
 
-func nextStr() string {
-	sc.Scan()
-	return sc.Text()
-}
-
-func pow(x, y int) int {
-	return int(math.Pow(float64(x), float64(y)))
-}
-
 func min(x, y int) int {
 	return int(math.Min(float64(x), float64(y)))
 }
 
-func max(x, y int) int {
-	return int(math.Max(float64(x), float64(y)))
-}
-
-// 数値配列をuniqして返す
-func uniq(input []int) (uniq []int) {
-	m := make(map[int]bool)
-	for _, elm := range input {
-		if !m[elm] {
-			m[elm] = true
-			uniq = append(uniq, elm)
-		}
-	}
-	return uniq
-}
-
 func init() {
 	sc.Split(bufio.ScanWords)
-}
-
-// 等差数列の和
-// ただし初項a、項数n、末項lがわかっている場合
-func sumArithmeticProgression_l(n, a, l int) int {
-	fmt.Println("n, a, l: ", n, a, l)
-	return n * (a + l) / 2
-}
-
-// 等差数列の和
-// ただし初項a、項数n、公差dがわかっている場合
-func sumArithmeticProgression_d(n, a, d int) int {
-	return (n / 2) * (2*a + (n-1)*d)
-}
-
-func mint(mod, n int64) int64 {
-	mint := big.NewInt(int64(n))
-	return mint.Mod(mint, big.NewInt(mod)).Int64()
 }
 
 // TODO テストケース通らずのコード
