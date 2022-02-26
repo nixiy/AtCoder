21afd87b77f805853f00222214a04f6457369459
Nix <vrcnix@gmail.com>
2022-02-26 11:26:41 +0900

    [add] 235 c

--- abc/abc235/c.go
+++ abc/abc235/c.go
@@ -95,6 +95,11 @@ func main() {
 	seq := make([]int, seqLen)
 	x := make([]int, pairLen)
 	k := make([]int, pairLen)
+	// 1 1 2 3 1 2 の場合
+	// 以下の様な数値がそれぞれ何番目に登場するのかを記録しておく
+	// 2次元配列で宣言すると10億x10億の配列を作ることになり大変な事になる(やってしまった)
+	// map[0:[1 2 5] 1:[3 6] 2:[4]]
+	mat := make(map[int][]int)
 
 	for i := 0; i < seqLen; i++ {
 		seq[i] = nextInt()
@@ -104,22 +109,22 @@ func main() {
 		k[i] = nextInt()
 	}
 
-	// 全探索
+	for i := 0; i < seqLen; i++ {
+		// matrix[今見ている数]にi+1を追加する
+		mat_i := seq[i] - 1
+		mat[mat_i] = append(mat[mat_i], i+1)
+	}
+
+	fmt.Println(mat)
+
 	for pi := 0; pi < pairLen; pi++ {
-		// xがk回目に出てくるのは何番目か
-		c := 0
-		for si := 0; si < seqLen; si++ {
-			// 数列とxの箇所が同じ
-			if seq[si] == x[pi] {
-				c++
-				// xの登場回数がk回目であれば
-				if c == k[pi] {
-					fmt.Println(si + 1)
-					break
-				}
-			}
-		}
-		if c == 0 || c < k[pi] {
+		xp := x[pi] - 1
+		kp := k[pi] - 1
+		// 対象の数値がseqに存在するか
+		// 対象のkpが配列のindex内に収まるか
+		if len(mat[xp]) != 0 && len(mat[xp]) > kp {
+			fmt.Println(mat[xp][kp])
+		} else {
 			fmt.Println(-1)
 		}
 	}
